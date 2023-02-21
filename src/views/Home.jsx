66a7db206f481e0ee160da738dba5ea7d0b19541
AlejoR27 <alejandrobr27@hotmail.com>
2023-02-21 15:34:55 +0100

    Adding to Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Login from '../components/Login'
+import Button from '../components/Button'
+import Navbar from '../components/Navbar'
 
 function Home() {
   return (
     <div>
+        <Navbar/>
         <h1>Login</h1>
         <Login />
+        <Button/>
     </div>
   )
 }
