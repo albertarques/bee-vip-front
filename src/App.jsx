e1301c54c38047b36489c2948dbfd309a07a4e46
Albert Arques <albertarques@gmail.com>
2023-02-21 12:16:33 +0100

    Refactored App delete unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css'
 
-import Login from './components/Login'
 import PublicRoutes from './router/PublicRoutes'
-import Home from './views/Home'
 
 
 
 function App() {
 
   return (
-    <>
       <PublicRoutes/>
-    </>
   )
 }
 
