3c0ebdd65695b3e2b53042738f130bbc2fa8d7d3
Francisco Aguirr <fran.uru.aguirre@gmail.com>
2023-02-21 11:42:53 +0100

    folder organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import Login from './components/Login'
+import Home from './views/Home'
+
+
 
 function App() {
 
   return (
     <>
+   <Login></Login>
       <Router>
         <Routes>
           <Route path='/' element={ <Dashboard /> } />
           <Route path='/entrepreneurships' element={ <Dashboard /> } />
+          <Route path='/home' element={ <Home /> } />
         </Routes>
-      </Router>
+      </Router> 
     </>
   )
 }
