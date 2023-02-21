6bdb935477ca3ec28436f786610ecb39e7794e67
AlejoR27 <alejandrobr27@hotmail.com>
2023-02-21 11:29:54 +0100

    buttonsComponents Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import Buttons from './components/Buttons'
 
 function App() {
 
   return (
     <>
-      <Router>
+
+    <Buttons/>
+      {/* <Router>
         <Routes>
           <Route path='/' element={ <Dashboard /> } />
           <Route path='/entrepreneurships' element={ <Dashboard /> } />
         </Routes>
-      </Router>
+      </Router> */}
     </>
   )
 }
