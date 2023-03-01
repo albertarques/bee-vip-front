1198a1f957d6fe7af6c21428f39659e41444d215
Francisco Aguirr <fran.uru.aguirre@gmail.com>
2023-03-01 19:15:13 +0100

    login form labels and input names added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,6 +4,8 @@ const Login = () => {
   return (
     <> 
     <div className="container mx-auto px-4">
+        <label class="block mb-2 text-lg font-medium text-white-900 dark:text-white" for="user_avatar">Iniciar sesión</label>
+        <br />
         <div class="relative">
           <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="red" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
