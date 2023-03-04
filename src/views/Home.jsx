4f4624ee5c02835e30a8a10af43a15b5a41b2ccc
Guillem <guillemmj33@gmail.com>
2023-03-04 12:12:21 +0100

    modifying develop 2

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -32,17 +32,11 @@ export default function Home() {
   }, [])
 
   return (
-    <div>
-      <h1>home</h1>
-      <div className="font-sans bg-slate-50">
-        <Navbar/>
-        <section className="bg-blue-800 w-full p-2 sm:mx-auto sm:px-20">
-          <h1 className="text-xl mt-2 mb-4 text-slate-100">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt
-            illo quidem.
-          </h1>
-          <Searchbar />
-        </section>
+    <div className="font-sans bg-slate-50">
+      <section className="bg-blue-800 w-full p-2 sm:mx-auto sm:px-20">
+        <h1 className="text-xl mt-2 mb-4 text-slate-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt illo quidem.</h1>
+        <Searchbar />
+      </section>
 
       <section className="container mt-4 mb-4 px-2 mx-auto">
         <h2 className="text-base font-medium font-title">Emprendimientos populares</h2>
@@ -67,12 +61,9 @@ export default function Home() {
           ))}
         </div>
 
-          <h2 className="mt-6 text-base font-medium font-title">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.
-          </h2>
-          <InfoHome />
-        </section>
-      </div>
+        <h2 className="mt-6 text-base font-medium font-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, aut.</h2>
+        <InfoHome />
+      </section>
     </div>
-  );
+  )
 }
