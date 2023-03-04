9d62b28e849f17d689077a724fc4ba40e79978fa
Albert Arques <albertarques@gmail.com>
2023-03-04 17:29:24 +0100

    Create EntrepreneurshipService

---
+++ src/apiService/entrepreneurshipService.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+const ENTREPRENEURSHIPS_URL = 'http://localhost:8000/api/entrepreneurships'
+
+const getEntrepreneurshipData = async () => {
+  try{
+    const response = await axios.get(ENTREPRENEURSHIPS_URL);
+    console.log('Data ok on EntrepreneurshipsService');
+    return response;  
+  } catch($err){
+    return $err;
+  }
+};
+
+export {
+  getEntrepreneurshipData,
+}
