99336587496eedcefe1ab6ac7d72479b41790b4d
Albert Arques <albertarques@gmail.com>
2023-03-04 10:52:14 +0100

    Added CategoryHomeCard import

--- src/views/SingleEntrepreneurship.jsx
+++ src/views/SingleEntrepreneurship.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import CategoryHomeCard from '../components/partials/CategoryCard/CategoryHomeCard'
 
 export default function SingleEntrepreneurship() {
   return (
